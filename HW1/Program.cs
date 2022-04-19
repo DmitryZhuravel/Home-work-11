bf0a57b57946bbb043cd131f44521ce69ab5e0fa
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-04-19 22:56:25 +0500

    Степень двойки

---
+++ HW1/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 1: Определите, является ли число степенью двойки:
+N = 16 -> "Является степень двойки"
+N = 12 -> “Не является степенью двойки”*/
+int Promt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+void DegreeTwo(int N)
+{
+    if (N / 2 == 0)
+    {
+        Console.WriteLine("Является степень двойки");
+        return;
+    }
+    if (N % 2 == 0)
+    {
+        DegreeTwo(N / 2);
+    }
+    else { Console.WriteLine("Не является степенью двойки"); }
+
+}
+
+int N = Promt("Введите N => ");
+DegreeTwo(N);
